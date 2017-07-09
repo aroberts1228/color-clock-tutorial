2fe73c9d51211772100d613b475c3cdcd2e5409e
Anthony Roberts <aroberts1228@gmail.com>
2017-07-08 22:18:41 -0500

    Add code for clock function

--- clock.js
+++ clock.js
@@ -1,14 +1,27 @@
 function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+	changeBackground(h, m, s);
+    // m = checkTime(m);
+    // s = checkTime(s);
+    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
+    setTimeout(function(){startTime()},500);
+}
 
-  changeBackground(h, m, s);
+function changeBackground(h, m, s){
+	// for smooth colour change
+	// work out total minutes
+	// and change hue based on minutes passed
+	var totalMins = (h * 60) + m;
+	var hue = totalMins * 0.25;
 
-  m = checkTime(m);
-  s = checkTime(s);
+	// for saturation we'll just add twenty to keep it in the mid ranges
+	var sat = m + 20;
 
-  document.getElementById('clock').innerHTML = h+":"+m+":"+s;
-  setTimeout(function(){startTime()},500);
+	// and the same of the lightness
+	var light = s + 15;
+
+	document.body.style.backgroundColor= "hsl(" + hue + ", " + sat + "%, " + light + "%)";
 }
