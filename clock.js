3872e29ebe1d895d925069cf1a3f368d35baa191
Anthony Roberts <aroberts1228@gmail.com>
2017-07-08 22:21:03 -0500

    add prefix "0" when number is under 10

--- clock.js
+++ clock.js
@@ -4,8 +4,8 @@ function startTime() {
     var m=today.getMinutes();
     var s=today.getSeconds();
 	changeBackground(h, m, s);
-    // m = checkTime(m);
-    // s = checkTime(s);
+    m = checkTime(m);
+    s = checkTime(s);
     document.getElementById('clock').innerHTML = h+":"+m+":"+s;
     setTimeout(function(){startTime()},500);
 }
@@ -24,4 +24,9 @@ function changeBackground(h, m, s){
 	var light = s + 15;
 
 	document.body.style.backgroundColor= "hsl(" + hue + ", " + sat + "%, " + light + "%)";
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
