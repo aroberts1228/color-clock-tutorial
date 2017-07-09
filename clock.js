f42aad954a764a9351349d82af3c50ff0d9d712c
Anthony Roberts <aroberts1228@gmail.com>
2017-07-08 21:56:35 -0500

    Create JavaScript File

--- clock.js
+++ clock.js
@@ -1,1 +1,14 @@
-js
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+
+  changeBackground(h, m, s);
+
+  m = checkTime(m);
+  s = checkTime(s);
+
+  document.getElementById('clock').innerHTML = h+":"+m+":"+s;
+  setTimeout(function(){startTime()},500);
+}
